b998f60b55217eb00cb8bc174380753de506ec7e
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-02-20 08:21:01 -0800

    initial migration

--- db/migrate/20220220153823_create_recipes.rb
+++ db/migrate/20220220153823_create_recipes.rb
@@ -1,7 +1,8 @@
 class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
-
+      t.string :name
+      t.string :description
       t.timestamps
     end
   end
