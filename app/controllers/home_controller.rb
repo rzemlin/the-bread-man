4b395812123c94d8fb431f47305f263b6ed76066
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-02-20 09:08:42 -0800

    add bootstrap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,9 @@ class HomeController < ApplicationController
 
     def index
     end
+
+    def about
+    end
     
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :recipes
   devise_for :users
   root to: 'home#index'
+  get '/about', to: 'home#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
