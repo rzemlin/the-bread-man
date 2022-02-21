e42f0b62f8af9f71e416bbb7eba8fb3da40ad1b9
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-02-21 15:56:51 -0800

    add users controller outside of devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+    def show
+
+    end
+
+    def index
+        
+        #render '/users/index'
+
+    end
+
+    def edit
+
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   
   resources :ingredients
   resources :recipes
-  
+  resources :users
   root to: 'home#index'
   get '/about', to: 'home#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
