d18ac5c47fa94652eb97f9981400cd5e49c7d4c0
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-02-26 06:39:52 -0800

    modify model associations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,4 @@
 class Ingredient < ApplicationRecord
-    has_many :recipes
-    has_many :users, through: :recipes
-    belongs_to :recipes
+    has_many :recipe_ingredients
+    has_many :recipes, through: :recipe_ingredients
 end
